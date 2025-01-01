ceb8afdb33f349730baedb61102633f5f459828f
geo0125 <stopfive0125@gmail.com>
2025-01-02 06:10:53 +0900

    [추가] 0102

--- practice.py
+++ practice.py
@@ -1103,58 +1103,161 @@ print(sample(lst,1)) # 랜덤뽑기 4
 
 # <클래스>-------------------------------------------------------------------------------
 # ★  개념
-# - 클래스(자동차 틀), 객체(자동차), 멤버변수(연료, 부품), 메서드(주행, 브레이크)
+# - 클래스(자동차 설계도), 객체(자동차), 멤버변수(속도, 브랜드), 메서드(주행, 브레이크)
+# - 상속(타이어, 엔진), 메서드 오버라이딩(가스차나 전기차 둘다 주행(drive)오버라이딩 )
 # 0) 클래스 없이는 객체 생성 시 중복 소스 및 비효율 변수선언 발생(효율성 저하)
 # 1) 클래스 선언 - 객체 생성
 # a. 클래스 paramater만큼 객체 생성시 argument 값 필요
+# 2) 멤버변수 외부 접근 가능
+# a. 외부에서 객체의 멤버변수 확장 가능
+# 3) 메소드
+# 4) 상속 : 타 클래스의 멤버변수, 메소드를 받음 (받는 쪽은 자식클래스, 그 역은 부모클래스)
+# 5) 다중상속 : 여러 부모클래스를 상속받음.
+# ex) Unit <- AttackUnit, Flyable.     AttackUnit, Flyabnle <- FlyableAttackUnit
 
-# 0)
-# 마린 : 공격 유닛, 군인. 총을 쏠 수 있음
-# 비효율 변수
-name = "마린"  # 유닛의 이름
-hp = 40  # 유닛의 체력
-damage = 5  # 유닛의 공격력
-# 중복 소스
-print("{0} 유닛이 생성되었습니다".format(name))
-print("체력 {0}, 공격력 {1}\n".format(hp, damage))
+# # 0)
+# # 마린 : 공격 유닛, 군인. 총을 쏠 수 있음
+# # 비효율 변수
+# name = "마린"  # 유닛의 이름
+# hp = 40  # 유닛의 체력
+# damage = 5  # 유닛의 공격력
+# # 중복 소스
+# print("{0} 유닛이 생성되었습니다".format(name))
+# print("체력 {0}, 공격력 {1}\n".format(hp, damage))
 
-# 탱크 : 공격 유닛, 탱크. 포를 쏠 수  있는데, 일반모드 / 시즈모드.
-# 비효율 변수
-tank_name = "탱크"
-tank_hp = 150
-tank_damage = 35
-# 중복 소스
-print("{0} 유닛이 생성되었습니다".format(tank_name))
-print("체력 {0}, 공격력 {1}\n".format(tank_hp, tank_damage))
-
-
-def attack(name, location, damage):
-    print("{0} : {1} 방향으로 적군을 공격합니다. [공격력 {2}]".format(
-        name, location, damage))
+# # 탱크 : 공격 유닛, 탱크. 포를 쏠 수  있는데, 일반모드 / 시즈모드.
+# # 비효율 변수
+# tank_name = "탱크"
+# tank_hp = 150
+# tank_damage = 35
+# # 중복 소스
+# print("{0} 유닛이 생성되었습니다".format(tank_name))
+# print("체력 {0}, 공격력 {1}\n".format(tank_hp, tank_damage))
 
 
-attack(name, "1시", damage)
-attack(tank_name, "1시", tank_damage)
-tank2_name = "탱크2"
-tank2_hp = 150
-tank2_damage = 35
-# 중복 소스
-print("{0} 유닛이 생성되었습니다".format(tank2_name))
-print("체력 {0}, 공격력 {1}\n".format(tank2_hp, tank2_damage))
-attack(tank_name, "1시", tank2_damage)
+# def attack(name, location, damage):
+#     print("{0} : {1} 방향으로 적군을 공격합니다. [공격력 {2}]".format(
+#         name, location, damage))
+
+
+# attack(name, "1시", damage)
+# attack(tank_name, "1시", tank_damage)
+# tank2_name = "탱크2"
+# tank2_hp = 150
+# tank2_damage = 35
+# # 중복 소스
+# print("{0} 유닛이 생성되었습니다".format(tank2_name))
+# print("체력 {0}, 공격력 {1}\n".format(tank2_hp, tank2_damage))
+# attack(tank_name, "1시", tank2_damage)
 
 # 1)
-
-
+# 일반 유닛
 class Unit:
-    def __init__(self, name, hp, damage):  # a)
-        self.name = name
+    def __init__(self, name, hp, speed):  # a)
+        self.name = name  # 멤버변수 : self.name, hp, ..
         self.hp = hp
+        self.speed = speed
+
+    def move(self, location):
+        print("[지상 유닛 이동]")
+        print("{0} : {1} 방향으로 이동합니다. [속도 {2}]".format(
+            self.name, location, self.speed))
+
+
+# marine1 = Unit("마린", 40, 5)
+# marine2 = Unit("마린", 40, 5)
+# tank1 = Unit("탱크", 150, 35)
+
+# # 2)
+# # 레이스 : 공중유닛, 비행기. 클로킹(상대방에게 보이지 않음)
+# wraith1 = Unit("레이스", 80, 5)
+# print("유닛 이름 :{0}, 공격력 :{1}".format(wraith1.name, wraith1.damage))
+
+# # 마인드 컨트롤 : 상대방 유닛을 내 것으로 만드는 것 (빼앗음)
+# wraith2 = Unit("레이스", 80, 5)
+# # a.
+# wraith2.clocking = True
+
+# if wraith2.clocking == True:
+#     print("{0} 는 현재 클로킹 상대입니다.".format(wraith2.name))
+
+# # 에러 발생
+# if wraith1.clocking == True:
+#     print("{0} 는 현재 클로킹 상대입니다.".format(wraith2.name))
+
+# 3)
+# 4)
+# 공격 유닛
+class AttackUnit(Unit):
+    def __init__(self, name, hp, speed, damage):
+        # 4)
+        # self.name = name
+        # self.hp = hp
+        Unit.__init__(self, name, hp, speed)
         self.damage = damage
-        print("{0} 유닛이 생성 되었습니다.".format(self.name))
-        print("체력 {0}, 공격력 {1}".format(self.hp, self.damage))
+
+    def attack(self, location):
+        print("{0} : {1} 방향으로 적군을 공격 합니다. [공격력 {2}]".format(
+            self.name, location, self.damage))
+
+    def damaged(self, damage):
+        print("{0} : {1} 데미지를 입었습니다.".format(self.name, damage))
+        self.hp -= damage
+        print("{0} : 현재 체력은 {1}입니다".format(self.name, self.hp))
+        if self.hp <= 0:
+            print("{0} : 파괴되었습니다.".format(self.name))
 
 
-marine1 = Unit("마린", 40, 5)
-marine2 = Unit("마린", 40, 5)
-tank1 = Unit("탱크", 150, 35)
+# # 파이어뱃 : 공격 유닛, 화염방사기.
+# firebat1 = AttackUnit("파이어뱃", 50, 16)
+# firebat1.attack("5시")
+
+# # 공격 2번 받는다고 가정
+# firebat1.damaged(25)
+# firebat1.damaged(25)
+
+
+# 드랍쉽 : 공중 유닛, 수송기. 마린 / 파이어뱃/ 탱크 등을 수송. 공격 X
+# 날 수 있는 기능을 가진 클래스
+class Flyable:
+    def __init__(self, flying_speed):
+        self.flying_speed = flying_speed
+
+#    메소드 오버라이딩
+    def fly(self, name, location):
+        print("{0} : {1} 방향으로 날아갑니다. [속도 {2}]".format(
+            name, location, self.flying_speed))
+
+# 5)
+# 공중 공격 유닛 클래스
+
+
+class FlyableAttackUnit(AttackUnit, Flyable):
+    def __init__(self, name, hp, damage, flying_speed):
+        AttackUnit.__init__(self, name, hp, 0, damage)  # 지상 speed 0
+        Flyable.__init__(self, flying_speed)
+
+    def move(self, location):
+        print("[공중 유닛 이동]")
+        self.fly(self.name, location)
+
+
+# # 발키리 : 공중 공격 유닛, 한 번에 14발 미사일 발사
+# valkyrie = FlyableAttackUnit("발키리", 200, 6, 5)
+# valkyrie.fly(valkyrie.name, "3시")
+
+
+# 6)
+# 벌쳐 : 지상유닛, 기동성이 좋음.
+vulture = AttackUnit("벌쳐", 80, 10, 20)
+
+# 배틀 크루저 : 공중 유닛, 체력 높고 공격력 높음
+battlecruiser = FlyableAttackUnit("배틀 크루저", 500, 25, 3)
+
+# 매번 지상유닛인지 공중유닛인지 확인하고 그에 따라 move, fly를 써야하는 불편함이 있음
+# vulture.move("11시")
+# battlecruiser.fly(battlecruiser.name, "9시")
+
+# 메소드 오버로딩 후
+vulture.move("11시")
+battlecruiser.move("9시")
